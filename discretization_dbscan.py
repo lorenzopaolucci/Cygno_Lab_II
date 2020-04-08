e161e04fb0355132e05245691fea9138ec95bfc0
lorenzopaolucci <62110267+lorenzopaolucci@users.noreply.github.com>
2020-04-08 19:00:03 +0200

    Update discretization_dbscan.py
    - Aggiunta iterazione threshold

--- discretization_dbscan.py
+++ discretization_dbscan.py
@@ -15,253 +15,251 @@ print_eff=0    #if !=0 print efficienza per ogni run
 
 
 L = 100             # Dimensione griglia
-th = 1.5            # Soglia
 centers = [[49,49]] # Centro del blob
 n_samples = 1000    # Numero di segnali generati
 sigma = 10          # Sigma per la generazione del blob
 
 
-# Genero L x L pixel secondo una gaussiana mu=0, sigma=1
+for th in np.arange(0.5,3,0.5):
 
-grid = np.zeros((L,L))     # Contiene le "coordinate" di ogni pixel, e il valore del segnale 
+    # Genero L x L pixel secondo una gaussiana mu=0, sigma=1
 
-background = 0             # Contatore per il fondo
+    grid = np.zeros((L,L))     # Contiene le "coordinate" di ogni pixel, e il valore del segnale
 
-for i in np.arange(0,L,1): # Riempio i pixel di un background uniforme
+    background = 0             # Contatore per il fondo
 
-  for j in np.arange(0,L,1):
+    for i in np.arange(0,L,1): # Riempio i pixel di un background uniforme
 
-    bkg = np.random.normal(0,1) 
+      for j in np.arange(0,L,1):
 
-    if bkg < th:           # Solo se sopra soglia il segnale viene considerato
+        bkg = np.random.normal(0,1)
 
-      grid[i][j] = 0
+        if bkg < th:           # Solo se sopra soglia il segnale viene considerato
 
-    else:
+          grid[i][j] = 0
 
-      grid[i][j] = 1
-
-      background+=1
-
-# Genero il blob
-
-X, blob_labels = make_blobs(n_samples=n_samples, centers=centers, cluster_std=sigma, random_state=0)
-
-# Discretizzazione del blob
-
-X_discrete = np.zeros_like(X)
-
-for i in np.arange(0,2,1):
-
-  for j in np.arange(0,len(X),1):
-
-    X_discrete[j][i] = np.around(X[j][i]) 
-
-# Riempio la griglia
-
-signal = 0                      # Contatore di quanto segnale "nuovo" viene aggiunto sul fondo
-
-for j in np.arange(0,len(X),1):
-
-  x = int(X_discrete[j][0])     # np.around ha come output lo stesso tipo dell'input, quindi float 
-  y = int(X_discrete[j][1])
-
-  if grid[x][y] == 0:
-
-    grid[x][y] = 1
-    
-    signal+=1
-
-# Plot di segnale+rumore & creazione array coordinate
-
-points_list= []
-
-for i in np.arange(0,L,1):    
-
-  for j in np.arange(0,L,1):
-
-    if grid[i][j] != 0:
-
-      plt.plot(j,i,'.', markeredgecolor='r', markersize=5)
-
-      x = j
-      y = i
-      points_list.append([x,y])
-      
-points = np.array(points_list)
-
-fig = matplotlib.pyplot.gcf()
-fig.set_size_inches(10, 10)
-plt.show()
-
-print('Background points: %d\nNew signal over background (out of %d generated): %d' %(background,n_samples,signal))
-
-
-# STUDIO EFFICIENZA DI DBSCAN
-
-
-##############################################################################
-
-
-for eps in np.arange(1, 10, 0.5):
-
-    # plot
-    eps_range = []
-    efficiency_noise_list = []
-    efficiency_cluster_list = []
-    min_samples_range = []
-
-
-    for min_samples in np.arange(2, 16, 1):
-
-
-        #plot_3d
-        eps_range.append(eps)
-        min_samples_range.append(min_samples)
-
-
-        db = DBSCAN(eps, min_samples).fit(points)                   # CLUSTERING
-        core_samples_mask = np.zeros_like(db.labels_,dtype=bool)    # Inizializza un array booleano, della stessa forma di labels_
-        core_samples_mask[db.core_sample_indices_] = True           # Considera tutti i core trovati da dbscan
-        labels = db.labels_
-        
-        
-        n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0) # Conta i cluster, togliendo il  rumore (k=-1)
-        n_noise_ = list(labels).count(-1)                           # Numero di punti di rumore
-        expected_noise = signal+background-n_samples
-
-        if info_cluster != 0:
-          print('Estimated number of clusters: %d' % n_clusters_)
-          print('Estimated number of noise points: %d' % n_noise_)
-
-        ##############################################################################
-        # Efficienza
-        if n_noise_ <= expected_noise:
-          eff_noise = 1 - (expected_noise-n_noise_)/(expected_noise)
-          
         else:
-          eff_noise = 1- (n_noise_-expected_noise)/(n_samples)
 
-        efficiency_noise_list.append(eff_noise)
-        eff_cluster = 0
-        weight = 0
-  
-        ##############################################################################
+          grid[i][j] = 1
 
-        import matplotlib.pyplot as plt
+          background+=1
 
-        unique_labels = set(labels)
-        colors = [plt.cm.Spectral(each)
-        for each in np.linspace(0, 1, len(unique_labels))] # Sceglie la palette di   colori senza il nero
+    # Genero il blob
 
-        for k, col in zip(unique_labels, colors):          # Per ogni cluster, associo un colore
-            if k == -1:
-              col = [0, 0, 0, 1]                           # Nero per il rumore
+    X, blob_labels = make_blobs(n_samples=n_samples, centers=centers, cluster_std=sigma, random_state=0)
+
+    # Discretizzazione del blob
+
+    X_discrete = np.zeros_like(X)
+
+    for i in np.arange(0,2,1):
+
+      for j in np.arange(0,len(X),1):
+
+        X_discrete[j][i] = np.around(X[j][i])
+
+    # Riempio la griglia
+
+    signal = 0                      # Contatore di quanto segnale "nuovo" viene aggiunto sul fondo
+
+    for j in np.arange(0,len(X),1):
+
+      x = int(X_discrete[j][0])     # np.around ha come output lo stesso tipo dell'input, quindi float
+      y = int(X_discrete[j][1])
+
+      if grid[x][y] == 0:
+
+        grid[x][y] = 1
         
-            class_member_mask = (labels == k)              # Seleziona tutti i punti del cluster k
+        signal+=1
 
-            xy_core = points[class_member_mask & core_samples_mask]    # Solo se è nel cluster E è un core point
-            xy_border = points[class_member_mask & ~core_samples_mask] # Solo se è nel cluster E non è core  ==  è un edge point del cluster
+    # Plot di segnale+rumore & creazione array coordinate
+
+    points_list= []
+
+    for i in np.arange(0,L,1):
+
+      for j in np.arange(0,L,1):
+
+        if grid[i][j] != 0:
+
+          plt.plot(j,i,'.', markeredgecolor='r', markersize=5)
+
+          x = j
+          y = i
+          points_list.append([x,y])
+          
+    points = np.array(points_list)
+
+    fig = matplotlib.pyplot.gcf()
+    fig.set_size_inches(10, 10)
+    plt.show()
+
+    print('Background points: %d\nNew signal over background (out of %d generated): %d' %(background,n_samples,signal))
+
+
+    # STUDIO EFFICIENZA DI DBSCAN
+
+
+    ##############################################################################
+
+
+    for eps in np.arange(1, 10, 0.5):
+
+        # plot
+        eps_range = []
+        efficiency_noise_list = []
+        efficiency_cluster_list = []
+        min_samples_range = []
+
+
+        for min_samples in np.arange(2, 16, 1):
+
+
+            #plot_3d
+            eps_range.append(eps)
+            min_samples_range.append(min_samples)
+
+
+            db = DBSCAN(eps, min_samples).fit(points)                   # CLUSTERING
+            core_samples_mask = np.zeros_like(db.labels_,dtype=bool)    # Inizializza un array booleano, della stessa forma di labels_
+            core_samples_mask[db.core_sample_indices_] = True           # Considera tutti i core trovati da dbscan
+            labels = db.labels_
             
+            
+            n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0) # Conta i cluster, togliendo il  rumore (k=-1)
+            n_noise_ = list(labels).count(-1)                           # Numero di punti di rumore
+            expected_noise = signal+background-n_samples
+
+            if info_cluster != 0:
+              print('Estimated number of clusters: %d' % n_clusters_)
+              print('Estimated number of noise points: %d' % n_noise_)
+
+            ##############################################################################
+            # Efficienza
+            if n_noise_ <= expected_noise:
+              eff_noise = 1 - (expected_noise-n_noise_)/(expected_noise)
+              
+            else:
+              eff_noise = 1- (n_noise_-expected_noise)/(n_samples)
+
+            efficiency_noise_list.append(eff_noise)
+            eff_cluster = 0
+            weight = 0
+      
+            ##############################################################################
+
+            import matplotlib.pyplot as plt
+
+            unique_labels = set(labels)
+            colors = [plt.cm.Spectral(each)
+            for each in np.linspace(0, 1, len(unique_labels))] # Sceglie la palette di   colori senza il nero
+
+            for k, col in zip(unique_labels, colors):          # Per ogni cluster, associo un colore
+                if k == -1:
+                  col = [0, 0, 0, 1]                           # Nero per il rumore
+            
+                class_member_mask = (labels == k)              # Seleziona tutti i punti del cluster k
+
+                xy_core = points[class_member_mask & core_samples_mask]    # Solo se è nel cluster E è un core point
+                xy_border = points[class_member_mask & ~core_samples_mask] # Solo se è nel cluster E non è core  ==  è un edge point del cluster
+                
+                if plot_cluster != 0:
+                  plt.plot(xy_core[:, 0], xy_core[:, 1], '.',
+                        markeredgecolor=tuple(col), markersize=5)
+
+                  plt.plot(xy_border[:, 0], xy_border[:, 1], '.',
+                        markeredgecolor=tuple(col), markersize=5)
+                
+                if k != -1 and n_clusters_ >= len(centers): # Solo se # cluster > # centri generati
+                  x = 0
+                  y = 0
+                  for ic in xy_core:
+                    x += ic[0]
+                    y += ic[1]
+                  for ib in xy_border:
+                    x += ib[0]
+                    y += ib[1]
+                  
+                  n_members=list(labels).count(k)           # Membri cluster k
+
+                  distance = []
+                  distance_from_centers = []
+
+                  for i_centers in centers:
+                    distance.append( np.sqrt( ((y/(n_members)-i_centers[1])**2
+                                                       + ((x/(n_members))-i_centers[0])**2 ))) # Dist media dei membri dai diversi centri generati
+
+                  distance_from_centers.append(min(distance))                                  # Centro più vicino
+
+                  expected = (signal-n_noise_)/len(centers)                                    # Num atteso di membri (meno il rumore)
+                  
+                  occurrence = (expected - abs(expected - (len(xy_border)+len(xy_core)))) / (expected) # Differenza rispetto all'atteso
+
+                  eff_cluster += occurrence / distance_from_centers[-1]                                # Eff_cluster: somma delle differenze pesate con 1/dist dal centro più vicino
+                  
+                  weight += 1/distance_from_centers[-1]
+                  
+                  if info_cluster !=0:
+                    print('\n\tcluster label %.lf'%(k))
+                    print('distanza del cluster da uno dei centri: %lf' %(distance_from_centers[-1]))
+                    print('occurences: %lf' %occurrence)
+                    print('elementi nel cluster: %lf' %(len(xy_border)+len(xy_core)))
+                    print('elementi in tutti i cluster: %lf' %(n_samples-n_noise_))
+                    print('somma numeratore efficienza cluster: %lf' %(eff_cluster))
+                    print('pesi: %lf' %(weight))
+                  
             if plot_cluster != 0:
-              plt.plot(xy_core[:, 0], xy_core[:, 1], '.', 
-                    markeredgecolor=tuple(col), markersize=5)
-
-              plt.plot(xy_border[:, 0], xy_border[:, 1], '.', 
-                    markeredgecolor=tuple(col), markersize=5)
+              plt.title('Eps=%.1lf, min_samples=%d, estimated number of clusters: %d' % (eps,min_samples,n_clusters_))
+              fig = matplotlib.pyplot.gcf()
+              fig.set_size_inches(10, 10)
+              plt.show()
             
-            if k != -1 and n_clusters_ >= len(centers): # Solo se # cluster > # centri generati
-              x = 0
-              y = 0
-              for ic in xy_core:
-                x += ic[0]
-                y += ic[1]
-              for ib in xy_border:
-                x += ib[0]
-                y += ib[1]
-              
-              n_members=list(labels).count(k)           # Membri cluster k
+            if  n_clusters_ >= len(centers):
+              eff_cluster /= weight
 
-              distance = []
-              distance_from_centers = []
+            efficiency_cluster_list.append(eff_cluster)
 
-              for i_centers in centers:
-                distance.append( np.sqrt( ((y/(n_members)-i_centers[1])**2
-                                                   + ((x/(n_members))-i_centers[0])**2 ))) # Dist media dei membri dai diversi centri generati
+            if print_eff != 0:
+              print('Noise efficiency: %.3lf \tCluster efficiency: %.3lf\n' %(eff_noise, eff_cluster))
 
-              distance_from_centers.append(min(distance))                                  # Centro più vicino
 
-              expected = (signal-n_noise_)/len(centers)                                    # Num atteso di membri (meno il rumore)
-              
-              occurrence = (expected - abs(expected - (len(xy_border)+len(xy_core)))) / (expected) # Differenza rispetto all'atteso
+        if plot_2d != 0:
+          plt.figure(1)
+          plt.title('Andamento Efficiency Noise in funzione di min_samples con eps=%lf' % eps)
+          plt.plot(min_samples_range,efficiency_noise_list, 'ro',min_samples_range,efficiency_noise_list,'k')
+          plt.xlabel('Min samples')
+          plt.ylabel('Efficiency Noise')
+          plt.xlim(-0.1, max(min_samples_range)+5)
 
-              eff_cluster += occurrence / distance_from_centers[-1]                                # Eff_cluster: somma delle differenze pesate con 1/dist dal centro più vicino
-              
-              weight += 1/distance_from_centers[-1]
-              
-              if info_cluster !=0:
-                print('\n\tcluster label %.lf'%(k))
-                print('distanza del cluster da uno dei centri: %lf' %(distance_from_centers[-1]))
-                print('occurences: %lf' %occurrence)
-                print('elementi nel cluster: %lf' %(len(xy_border)+len(xy_core)))
-                print('elementi in tutti i cluster: %lf' %(n_samples-n_noise_))
-                print('somma numeratore efficienza cluster: %lf' %(eff_cluster))
-                print('pesi: %lf' %(weight))
-              
-        if plot_cluster != 0:
-          plt.title('Eps=%.1lf, min_samples=%d, estimated number of clusters: %d' % (eps,min_samples,n_clusters_))
-          fig = matplotlib.pyplot.gcf()
-          fig.set_size_inches(10, 10) 
+          plt.figure(2) #Terry
+          plt.title('Andamento Efficiency Cluster in funzione di min_samples con eps=%lf' % eps)
+          plt.plot(min_samples_range,efficiency_cluster_list,'g^',min_samples_range,efficiency_cluster_list,'b')
+          plt.xlabel('Min samples')
+          plt.ylabel('Efficiency Cluster')
+          plt.xlim(-0.1, max(min_samples_range)+5)
           plt.show()
-        
-        if  n_clusters_ >= len(centers):
-          eff_cluster /= weight
 
-        efficiency_cluster_list.append(eff_cluster)
+        if info_cluster !=0 or plot_cluster !=0 or plot_2d !=0 or print_eff !=0:
+          print('\n ################################################################## \n')
 
-        if print_eff != 0:
-          print('Noise efficiency: %.3lf \tCluster efficiency: %.3lf\n' %(eff_noise, eff_cluster))
-          print('Total efficiency: %.3lf\n\n' %efficiency)
+    #if plot_3d != 0:
+     # fig = plt.figure()
+     # dx = np.full_like(eps_range, 0.04)
+     # dy = np.full_like(min_samples_range, 1)
+     # z = np.zeros_like(efficiency_list)
+     # ax = fig.add_subplot(111, projection='3d')
+     # plt.title('Efficiency Histogram', fontsize=20)
+     # plt.xlabel('eps', fontsize=15)
+     # plt.ylabel('min samples', fontsize=15)
+     # fig.set_size_inches(11,8)
+     # ax.bar3d(eps_range, min_samples_range, z, dx, dy, efficiency_list, color='g')
+     # plt.show()
 
+    #max_index = efficiency_list.index(max(efficiency_list))
+    #eps_best.append(eps_range[max_index])
+    #min_samples_best.append(min_samples_range[max_index])
+    #efficiency_best.append(max(efficiency_list))
 
-    if plot_2d != 0:
-      plt.figure(1)
-      plt.title('Andamento Efficiency Noise in funzione di min_samples con eps=%lf' % eps)
-      plt.plot(min_samples_range,efficiency_noise_list, 'ro',min_samples_range,efficiency_noise_list,'k')
-      plt.xlabel('Min samples')
-      plt.ylabel('Efficiency Noise')
-      plt.xlim(-0.1, max(min_samples_range)+5)
+    #print('The maximum value of efficiency is: %.3lf \tfor eps: %.2lf, min_samples: %d' %(max(efficiency_list), eps_range[max_index], min_samples_range[max_index]))
 
-      plt.figure(2) #Terry
-      plt.title('Andamento Efficiency Cluster in funzione di min_samples con eps=%lf' % eps)
-      plt.plot(min_samples_range,efficiency_cluster_list,'g^',min_samples_range,efficiency_cluster_list,'b')
-      plt.xlabel('Min samples')
-      plt.ylabel('Efficiency Cluster')
-      plt.xlim(-0.1, max(min_samples_range)+5)
-      plt.show()
-
-    if info_cluster !=0 or plot_cluster !=0 or plot_2d !=0 or print_eff !=0:
-      print('\n ################################################################## \n')
-
-#if plot_3d != 0:
- # fig = plt.figure()
- # dx = np.full_like(eps_range, 0.04)
- # dy = np.full_like(min_samples_range, 1)
- # z = np.zeros_like(efficiency_list)
- # ax = fig.add_subplot(111, projection='3d')
- # plt.title('Efficiency Histogram', fontsize=20)
- # plt.xlabel('eps', fontsize=15)
- # plt.ylabel('min samples', fontsize=15)
- # fig.set_size_inches(11,8)
- # ax.bar3d(eps_range, min_samples_range, z, dx, dy, efficiency_list, color='g')
- # plt.show()
-
-#max_index = efficiency_list.index(max(efficiency_list))
-#eps_best.append(eps_range[max_index])
-#min_samples_best.append(min_samples_range[max_index])
-#efficiency_best.append(max(efficiency_list))
-
-#print('The maximum value of efficiency is: %.3lf \tfor eps: %.2lf, min_samples: %d' %(max(efficiency_list), eps_range[max_index], min_samples_range[max_index]))
-
-  
-  
